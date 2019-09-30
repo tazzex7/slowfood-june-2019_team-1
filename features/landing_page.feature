5215cc6763858e5e72370f7712f2d0481c441658
jonas <jonas.masharqa@gmail.com>
2019-09-30 19:19:25 +0200

    Defines root path and generates landing controller.

---
+++ features/landing_page.feature
@@ -0,0 +1,9 @@
+Feature: User visits Food Comrade landing page
+  As a visitor
+  In order to navigate the site
+  I would like to use a navbar
+
+  Scenario: User visits landing page
+    When I visit the landing page
+    And I click "Menu"
+    Then I should see "Joe's pizza"
---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,11 @@
+When("I visit the landing page") do
+  visit root_path
+end
+
+When("I click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
