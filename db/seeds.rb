e243d70eb72e584d0fd3b6d1397712e3d00c83ff
yasmine <yasmine.ezequiel@gmail.com>
2019-10-01 13:14:24 +0200

    Adds products to seeds
    Co-authored-by: Pia von Wachenfelt <piavonwachenfelt@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create(name: "Garlic Bread", description: "garlic, bread, butter", price: 30) 
+product = Product.create(name: "Margherita", description: "cheese, tomato sauce, basil", price: 70)
+product = Product.create(name: "Vesuvio", description: "cheese, tomato sauce, ham", price: 70)
+product = Product.create(name: "Calzone", description: "cheese, tomato sauce, ham", price: 80)
+product = Product.create(name: "Ice-cream", description: "vanilla", price: 40)
+product = Product.create(name: "Ice-cream", description: "strawberry", price: 40)
