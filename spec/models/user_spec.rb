db791a62259e0c676dbd3c97f3f532ca8c10c7aa
clalil <cliljander@gmail.com>
2019-10-02 13:07:47 +0200

    Remove unneccessary validation from user model spec. Remove surplus sad paths to stay in scope.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of :name }
-    it { is_expected.to validate_uniqueness_of :name }
     it { is_expected.to validate_presence_of :email }
   end
   
