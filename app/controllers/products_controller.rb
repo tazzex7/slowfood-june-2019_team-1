b83286f3f44df9d2037047a7acee650123e68e88
clalil <cliljander@gmail.com>
2019-10-02 15:43:12 +0200

    Configure product controller to add update functionality to edit action.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,10 +7,25 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
   def delete
     @product = Product.find(params[:id])
     @product.destroy
 
     redirect_to root_path
   end
+
+  private 
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
 end
