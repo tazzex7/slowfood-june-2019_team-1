d946e3231bdbad0b549b1f4c6d8307cebddf6b59
David_Izosimov <David.Izosimov@gmail.com>
2019-10-03 11:46:35 +0200

    Removes out of scope edit and delete functionality from products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,5 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all
   end
-
-  before_action :authenticate_admin!, only: [:edit, :update, :destroy]
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
-      redirect_to root_path
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-
-    redirect_to root_path
-  end
-
-  private 
-    def product_params
-      params.require(:product).permit(:name, :description, :price)
-    end
 end
