7c4c5c54ec1cd3094a33dc8a6882d6ae5b84d6a0
David_Izosimov <David.Izosimov@gmail.com>
2019-10-03 11:50:45 +0200

    Removes out of scope controller functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_admin!
-    authenticate_user!
-    redirect_to root_path, status: :forbidden unless current_user.admin?
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
